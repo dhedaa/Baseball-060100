954449aa5d6a8bac72dc5b29eecefc7bb5b1cf05
donghyuck <dheda359@gmail.com>
2025-07-02 13:37:14 +0900

    [feature] 49

--- BaseBall/baseball.cpp
+++ BaseBall/baseball.cpp
@@ -2,10 +2,20 @@
 
 using namespace std;
 
+struct GuessResult {
+	bool solved;
+	int strikes;
+	int balls;
+};
+
 class Baseball {
 public:
-	void guess(const string& guessNumber) {
+
+	explicit Baseball(const string& question) : question(question) {}
+
+	GuessResult guess(const string& guessNumber) {
 		assertIllegalArgument(guessNumber);
+		return { true, 3, 0 };
 	}
 	void assertIllegalArgument(const std::string& guessNumber)
 	{
@@ -26,4 +36,7 @@ public:
 			|| guessNumber[1] == guessNumber[2]
 			|| guessNumber[2] == guessNumber[0];
 	}
+private:
+	string question;
 };
+
