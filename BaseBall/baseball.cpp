05a99bff2ec0fbc316716294c66ba61d008a7815
donghyuck <dheda359@gmail.com>
2025-07-02 13:25:39 +0900

    [feature] 39

--- BaseBall/baseball.cpp
+++ BaseBall/baseball.cpp
@@ -4,14 +4,19 @@ using namespace std;
 
 class Baseball {
 public:
-	void guess(const string& guessString) {
-		if (guessString.length() != 3) {
+	void guess(const string& guessNumber) {
+		if (guessNumber.length() != 3) {
 			throw length_error("Must be three letters");
 		}
-		for (char ch : guessString) {
+		for (char ch : guessNumber) {
 			if (ch < '0' || ch > '9') {
 				throw invalid_argument("Must be number");
 			}
+		}
+		if (guessNumber[0] == guessNumber[1]
+			|| guessNumber[1] == guessNumber[2]
+			|| guessNumber[2] == guessNumber[0]) {
+			throw invalid_argument("Must not have the same number");
 		}
 	}
 };
